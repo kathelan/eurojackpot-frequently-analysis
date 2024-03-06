14527d32794b5d653d7e3ad02c31a2af9c983e3d
kathelan <mateuszdeptulski@gmail.com>
2024-03-06 10:04:54 +0100

    Initial commit

---
+++ src/main/java/pl/kathelan/app/Application.java
@@ -0,0 +1,13 @@
+package pl.kathelan.eurojackpotfrequentlyanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurojackpotFrequentlyAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurojackpotFrequentlyAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kathelan/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.eurojackpotfrequentlyanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurojackpotFrequentlyAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
